8f8629138a892bfe88d1a159305587b4d0e462bc
Anna228322 <anna.fittsdzehrald@mail.ru>
2023-01-30 17:11:11 +0500

    2.4.8(явное ожидание)

--- selenium_env/Scripts/main.py
+++ selenium_env/Scripts/main.py
@@ -4,29 +4,33 @@ from selenium.webdriver.common.by import By
 import time
 import math
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-link = "http://suninjuly.github.io/redirect_accept.html"
+link = "http://suninjuly.github.io/explicit_wait2.html"
 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
 try:
     browser = webdriver.Chrome()
+#    browser.implicitly_wait(5)
     browser.get(link)
 
-
-    button = browser.find_element(By.CSS_SELECTOR, ".btn")
+    WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#price"), '100')
+    )
+    button = browser.find_element(By.CSS_SELECTOR, "#book")
     button.click()
 
-    new_window = browser.window_handles[1]
-    browser.switch_to.window(new_window)
+    browser.execute_script("window.scrollBy(0, 100);")
 
     x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
     y = calc(x)
     input = browser.find_element(By.CSS_SELECTOR, "#answer")
     input.send_keys(y)
 
-    submit = browser.find_element(By.CSS_SELECTOR, '[type="submit"]')
+    submit = browser.find_element(By.CSS_SELECTOR, "#solve")
     submit.click()
 
     print(browser.switch_to.alert.text)
