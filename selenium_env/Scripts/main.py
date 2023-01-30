a5b5a399578c7e81cdc9a67017a5c22cc86a48ce
Anna228322 <anna.fittsdzehrald@mail.ru>
2023-01-30 11:34:27 +0500

    2.2(lists)

--- selenium_env/Scripts/main.py
+++ selenium_env/Scripts/main.py
@@ -2,35 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import math
+from selenium.webdriver.support.ui import Select
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
-
-link = "http://suninjuly.github.io/get_attribute.html"
+link = "http://suninjuly.github.io/selects1.html"
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    x_img_element = browser.find_element(By.CSS_SELECTOR, "img")
-    x = x_img_element.get_attribute("valuex")
-    y = calc(x)
 
-    input = browser.find_element(By.CSS_SELECTOR, "[id='answer']")
-    input.send_keys(y)
+    num1 = browser.find_element(By.CSS_SELECTOR, "#num1").text
+    num2 = browser.find_element(By.CSS_SELECTOR, "#num2").text
+    sum = int(num1) + int(num2)
 
-    radio = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
-    radio.click()
-    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
-    checkbox.click()
+    select = Select(browser.find_element(By.TAG_NAME, "select"))
+    select.select_by_value(str(sum))
 
     button = browser.find_element(By.XPATH, '//button[text()="Submit"]')
     button.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
     time.sleep(5)
-    # закрываем браузер после всех манипуляций
     browser.quit()
 
 # не забываем оставить пустую строку в конце файла
