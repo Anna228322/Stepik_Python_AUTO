4490799f04b7a5cf3c518f9b0fdfccda895607d9
Anna228322 <anna.fittsdzehrald@mail.ru>
2023-01-30 15:50:05 +0500

    2.3.4(работа с алертами)

--- selenium_env/Scripts/main.py
+++ selenium_env/Scripts/main.py
@@ -1,25 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common import alert
 from selenium.webdriver.common.by import By
 import time
 import math
 from selenium.webdriver.support.ui import Select
 
-link = "http://suninjuly.github.io/selects1.html"
+link = "http://suninjuly.github.io/alert_accept.html"
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
 
 
-    num1 = browser.find_element(By.CSS_SELECTOR, "#num1").text
-    num2 = browser.find_element(By.CSS_SELECTOR, "#num2").text
-    sum = int(num1) + int(num2)
-
-    select = Select(browser.find_element(By.TAG_NAME, "select"))
-    select.select_by_value(str(sum))
-
-    button = browser.find_element(By.XPATH, '//button[text()="Submit"]')
+    button = browser.find_element(By.CSS_SELECTOR, ".btn")
     button.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    y = calc(x)
+    input = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input.send_keys(y)
+
+    submit = browser.find_element(By.CSS_SELECTOR, '[type="submit"]')
+    submit.click()
+
+    print(browser.switch_to.alert.text)
 
 finally:
     time.sleep(5)
